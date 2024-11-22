a198fc1106a8778fb106b9f6420d0e1a712318b9
Eduardo Butragueño Romón <lincografic@gmail.com>
2024-11-22 13:29:50 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,27 +1,11 @@
-//create web server
+// Create web server
+// Create a new web server using the express module. This server will listen on port 3000 and serve the files in the public directory.
 const express = require('express');
 const app = express();
-//parse incoming request
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+const path = require('path');
 
-//get comments
-app.get('/comments', (req, res) => {
-  //get comments from database
-  const comments = database.getComments();
-  //send comments back to client
-  res.json(comments);
-});
-//post comment
-app.post('/comments', (req, res) => {
-  //get comment from client
-  const comment = req.body.comment;
-  //save comment to database
-  database.saveComment(comment);
-  //send success status back to client
-  res.json({status: 'success'});
-});
-//start server
+app.use(express.static('public'));
+
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log('App is listening on port 3000');
 });
